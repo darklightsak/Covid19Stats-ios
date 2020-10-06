c757f01006298768f12cbf51759c9fe4e32061e2
darklightsak <s.wattanapradit@gmail.com>
2020-10-06 16:52:08 +0700

    setup project

---
+++ Covid19Stats/Covid19Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19Stats
+//
+//  Created by Surasak Wattanapradit on 6/10/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
